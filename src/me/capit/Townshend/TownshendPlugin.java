c0d1ad5e19359b6039d0ef1f4bbc4e4c1cdb1ba8
NukesForKids <matt@capit.me>
2014-10-20 18:02:33 -0700

    Reorganized and modified for other three commits.

--- src/me/capit/Townshend/TownshendPlugin.java
+++ src/me/capit/Townshend/TownshendPlugin.java
@@ -1,19 +1,24 @@
 package me.capit.Townshend;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.UUID;
 import java.util.logging.Logger;
 
 import me.capit.Townshend.aegis.Aegis;
+import me.capit.Townshend.aegis.Aegis.AegisCreationException;
 import me.capit.Townshend.group.Group;
+import me.capit.Townshend.group.Group.GroupCreationException;
 import me.capit.Townshend.group.GroupCommands;
 import me.capit.Townshend.town.Town;
 import me.capit.Townshend.town.Town.TownCreationException;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TownshendPlugin extends JavaPlugin {
@@ -39,6 +44,8 @@ public class TownshendPlugin extends JavaPlugin {
 			ChatColor.DARK_GRAY+"] "+ChatColor.WHITE;
 	
 	public static File townsDir = null;
+	public static File groupsDir = null;
+	public static File aegisDir = null;
 	
 	public TownCommands commands = null;
 	public GroupCommands gcmds = null;
@@ -56,10 +63,9 @@ public class TownshendPlugin extends JavaPlugin {
 		
 		townsDir = new File(this.getDataFolder().getAbsolutePath()+File.separator+"towns");
 		if (!townsDir.exists() || !townsDir.isDirectory()){
-			console.sendMessage(ChatColor.WHITE+"Looks like this is a first-time run. "+ChatColor.LIGHT_PURPLE+"Hang on...");
+			console.sendMessage(ChatColor.WHITE+"Towns directory missing. "+ChatColor.LIGHT_PURPLE+"Creating...");
 			townsDir.mkdir();
 		}
-		
 		for (File town : townsDir.listFiles()){
 			try {
 				Town t = new Town(this, town.getName());
@@ -67,7 +73,48 @@ public class TownshendPlugin extends JavaPlugin {
 				console.sendMessage(ChatColor.WHITE+" > Loaded data for "+ChatColor.LIGHT_PURPLE
 						+t.getName()+ChatColor.WHITE+" ("+ChatColor.AQUA+t.ID+ChatColor.WHITE+").");
 			} catch (TownCreationException e) {
-				e.printStackTrace();
+				console.sendMessage(ChatColor.WHITE+" > "+ChatColor.RED+"Error "+ChatColor.WHITE
+						+"loading Town from "+ChatColor.AQUA+town.getName()+ChatColor.WHITE+": "+e.getLocalizedMessage());
+			}
+		}
+		
+		groupsDir = new File(this.getDataFolder().getAbsolutePath()+File.separator+"groups");
+		if (!groupsDir.exists() || !groupsDir.isDirectory()){
+			console.sendMessage(ChatColor.WHITE+"Groups directory missing. "+ChatColor.LIGHT_PURPLE+"Creating...");
+			groupsDir.mkdir();
+		}
+		for (File group : groupsDir.listFiles()){
+			try {
+				Group g = new Group(this, group.getName());
+				groups.add(g);
+				console.sendMessage(ChatColor.WHITE+" > Loaded group "+ChatColor.LIGHT_PURPLE
+						+g.name+ChatColor.WHITE+".");
+			} catch (GroupCreationException e) {
+				console.sendMessage(ChatColor.WHITE+" > "+ChatColor.RED+"Error "+ChatColor.WHITE
+						+"loading Group from "+ChatColor.AQUA+group.getName()+ChatColor.WHITE+": "+e.getLocalizedMessage());
+			}
+		}
+		
+		aegisDir = new File(getDataFolder().getAbsolutePath()+File.separator+"aegis");
+		if (!aegisDir.exists() || !aegisDir.isDirectory()){
+			console.sendMessage(ChatColor.WHITE+"Aegis directory missing. "+ChatColor.LIGHT_PURPLE+"Creating...");
+			aegisDir.mkdir();
+		}
+		for (File aegisFile : aegisDir.listFiles()){
+			YamlConfiguration aConfig = YamlConfiguration.loadConfiguration(aegisFile);
+			Location center = new Location(
+					getServer().getWorld(aConfig.getString("world")),
+					aConfig.getDouble("x"), aConfig.getDouble("y"), aConfig.getDouble("z"));
+			try {
+				Aegis aegis = new Aegis(center, aConfig.getString("name"), aConfig.getInt("ID"));
+				aegises.add(aegis);
+				console.sendMessage(ChatColor.WHITE+" > Loaded data for Aegis "+ChatColor.LIGHT_PURPLE
+						+aegis.getGroup()+ChatColor.WHITE+" ("+ChatColor.AQUA
+						+center.getX()+","+center.getY()+","+center.getZ()+ChatColor.WHITE+").");
+			} catch (AegisCreationException e){
+				console.sendMessage(ChatColor.WHITE+" > "+ChatColor.RED+"Error "+ChatColor.WHITE
+						+"loading Aegis at ("+ChatColor.AQUA+center.getX()+","+center.getY()+","
+						+center.getZ()+ChatColor.WHITE+"): "+e.getLocalizedMessage());
 			}
 		}
 		
@@ -90,6 +137,26 @@ public class TownshendPlugin extends JavaPlugin {
 		
 		for (Town town : towns){
 			town.save();
+		}
+		
+		for (Group group : groups){
+			group.save();
+		}
+		
+		for (Aegis aegis : aegises){
+			try {
+				File aFile = new File(aegisDir+File.separator+aegis.ID+".yml");
+				YamlConfiguration aConfig = YamlConfiguration.loadConfiguration(aFile);
+				aConfig.set("ID", aegis.ID);
+				aConfig.set("world", aegis.getCenter().getWorld().getName());
+				aConfig.set("name", aegis.getGroup());
+				aConfig.set("x", aegis.getSignLoc().getX());
+				aConfig.set("y", aegis.getSignLoc().getY());
+				aConfig.set("z", aegis.getSignLoc().getZ());
+				aConfig.save(aFile);
+			} catch (IOException e){
+				e.printStackTrace();
+			}
 		}
 		
 		console.sendMessage(ChatColor.WHITE+"Wrapped up.");
